81516d198b98f5ef9ddfdeaeadd7b845e39a8c44
Josias <josiasduran51@gmail.com>
2025-01-11 23:22:32 -0400

    fix: allow nulls for update and delete company resolver

--- src/companies/companies.resolver.ts
+++ src/companies/companies.resolver.ts
@@ -6,7 +6,7 @@ import { UpdateCompanyInput } from "./domain/dtos/updateCompany.input";
 
 @Resolver()
 export class CompaniesResolver {
-	constructor(private readonly companiesService: CompaniesService) {}
+	constructor(private readonly companiesService: CompaniesService) { }
 	@Query(() => [Company])
 	Companies(): Company[] {
 		return this.companiesService.GetAll();
@@ -23,12 +23,12 @@ export class CompaniesResolver {
 		return this.companiesService.Create(input);
 	}
 
-	@Mutation(() => Company)
+	@Mutation(() => Company, { nullable: true })
 	UpdateCompany(@Args("updateCompanyInput") input: UpdateCompanyInput): void {
 		this.companiesService.UpdateCompany(input);
 	}
 
-	@Mutation(() => Company)
+	@Mutation(() => Company, { nullable: true })
 	DeleteCompany(@Args("id") id: number): void {
 		this.companiesService.Delete(id);
 	}
