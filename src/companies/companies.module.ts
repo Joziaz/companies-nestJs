578935c2ad6583643dd0b3d131022ea40c6d724a
Josias <josiasduran51@gmail.com>
2025-01-13 15:55:11 -0400

    refactor: create resolvers folder

--- src/companies/companies.module.ts
+++ src/companies/companies.module.ts
@@ -1,17 +1,17 @@
 import { Module } from "@nestjs/common";
 import { CompaniesService } from "./services/companies.service";
-import { CompaniesResolver } from "./companies.resolver";
 import { Company, CompanySchema } from "./domain/entities/company.entity";
-import { ClientResolver } from "./clients.resolver";
 import { ClientService } from "./services/clients.service";
 import { Client, ClientSchema } from "./domain/entities/client.entity";
 import { Employee, EmployeeSchema } from "./domain/entities/employee.entity";
 import { Task, TaskSchema } from "./domain/entities/task.entity";
 import { MongooseModule } from "@nestjs/mongoose";
 import { TaskService } from "./services/task.service";
-import { TaskResolver } from "./task.resolver";
 import { EmployeeService } from "./services/employee.service";
-import { EmployeeResolver } from "./employee.resolver";
+import { CompaniesResolver } from "./services/companies.resolver";
+import { ClientResolver } from "./services/clients.resolver";
+import { TaskResolver } from "./services/task.resolver";
+import { EmployeeResolver } from "./services/employee.resolver";
 
 @Module({
 	imports: [
